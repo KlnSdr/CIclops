4b2ee00e41daa4e29e28563c471d7aa281f00e19
KlnSdr <monsieurk1209@gmail.com>
2025-04-20 23:29:53 +0200

    runner launches container with ciclopsBuilder inside and logs output to stdout

--- src/ciclops/runner/Runner.java
+++ src/ciclops/runner/Runner.java
@@ -1,7 +1,11 @@
 package ciclops.runner;
 
+import ciclops.projects.Project;
+import ciclops.projects.service.ProjectsService;
 import common.logger.Logger;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.util.UUID;
 
 public class Runner {
@@ -16,13 +20,42 @@ public class Runner {
 
     public void start() {
         LOGGER.debug("Starting runner " + id);
+        final Project project = ProjectsService.getInstance().findById(projectId.toString());
+
+        if (project == null) {
+            LOGGER.warn("Project not found: " + projectId + ". Aborting build.");
+            return;
+        }
+
+        final String scmUrl = project.getGitUrl();
+        initBuildPod(scmUrl);
+    }
+
+    private void initBuildPod(String scmUrl) {
+        // TODO
+        // set env variables
+        // - scmURL
+        // - inject git credentials
+
+        final String command = "podman run --rm ciclopsbuilder:0.6";
         try {
-            Thread.sleep(10000 + (long) (Math.random() * 10000)); // Simulate build time
-        } catch (InterruptedException e) {
-            LOGGER.error("Runner interrupted: " + id);
-            Thread.currentThread().interrupt();
-        } finally {
-            LOGGER.debug("Runner " + id + " finished");
+            final ProcessBuilder processBuilder = new ProcessBuilder("sh", "-c", command);
+            processBuilder.redirectErrorStream(true);
+            final Process process = processBuilder.start();
+
+            final BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+            String line;
+            while ((line = reader.readLine()) != null) {
+                LOGGER.debug(line);
+            }
+
+            int exitCode = process.waitFor();
+            if (exitCode != 0) {
+                LOGGER.error("Failed to execute command \"" + command + "\". Exit code: " + exitCode);
+            }
+        } catch (Exception e) {
+            LOGGER.error("Error executing command: " + command);
+            LOGGER.trace(e);
         }
     }
 }
