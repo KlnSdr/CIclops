e17cfd6868876e61fb917b7a8da6714b795b199d
KlnSdr <monsieurk1209@gmail.com>
2025-04-21 16:25:51 +0200

    pass repo auth into buildpod and launch it

--- src/ciclops/runner/Runner.java
+++ src/ciclops/runner/Runner.java
@@ -3,9 +3,15 @@ package ciclops.runner;
 import ciclops.projects.Project;
 import ciclops.projects.service.ProjectsService;
 import common.logger.Logger;
+import dobby.util.json.NewJson;
 
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.util.Base64;
+import java.util.Comparator;
 import java.util.UUID;
 
 public class Runner {
@@ -28,7 +34,77 @@ public class Runner {
         }
 
         final String scmUrl = project.getGitUrl();
+        if (!createTmpDir()) {
+            LOGGER.error("Failed to create tmp dir for project " + project.getName());
+            return;
+        }
+        final String loginFileJson = generateLoginFile("USERNAME", "PASSWORD", "REPO");
+
+        if (!writeAuthFile(loginFileJson)) {
+            cleanup();
+            LOGGER.error("Failed to write auth file for project " + project.getName());
+            return;
+        }
         initBuildPod(scmUrl);
+        cleanup();
+    }
+
+    private boolean writeAuthFile(String content) {
+        final String authFilePath = "/tmp/ciclops/" + id + "/auth.json";
+        try {
+            final File authFile = new File(authFilePath);
+            authFile.createNewFile();
+            Files.writeString(authFile.toPath(), content);
+            return true;
+        } catch (Exception e) {
+            LOGGER.error("Failed to write auth file: " + authFilePath);
+            LOGGER.trace(e);
+            return false;
+        }
+    }
+
+    private boolean createTmpDir() {
+        final String tmpDir = "/tmp/ciclops/" + id;
+        final File tmpDirFile = new File(tmpDir);
+
+        if (tmpDirFile.exists()) {
+            return true;
+        }
+        return tmpDirFile.mkdirs();
+    }
+
+    private String generateLoginFile(String username, String password, String repoUrl) {
+        final NewJson registry = new NewJson();
+        registry.setString("auth", Base64.getEncoder().encodeToString((username + ":" + password).getBytes(StandardCharsets.UTF_8)));
+        final NewJson auths = new NewJson();
+        auths.setJson("REPO", registry);
+        final NewJson json = new NewJson();
+        json.setJson("auths", auths);
+
+        return json.toString().replace("REPO", repoUrl);
+    }
+
+    private void cleanup() {
+        final String tmpDir = "/tmp/ciclops/" + id;
+        final File tmpDirFile = new File(tmpDir);
+        if (!tmpDirFile.exists()) {
+            return;
+        }
+        try {
+            Files.walk(tmpDirFile.toPath())
+                    .sorted(Comparator.reverseOrder())
+                    .forEach(path -> {
+                        try {
+                            Files.delete(path);
+                        } catch (Exception e) {
+                            LOGGER.error("Failed to delete file: " + path);
+                            LOGGER.trace(e);
+                        }
+                    });
+        } catch (Exception e) {
+            LOGGER.error("Failed to delete tmp dir: " + tmpDir);
+            LOGGER.trace(e);
+        }
     }
 
     private void initBuildPod(String scmUrl) {
@@ -37,7 +113,7 @@ public class Runner {
         // - scmURL
         // - inject git credentials
 
-        final String command = "podman run --rm ciclopsbuilder:0.6";
+        final String command = "podman run --privileged -v /tmp/ciclops/" + id + "/auth.json:/root/.config/containers/auth.json:ro --rm ciclopsbuilder:0.16";
         try {
             final ProcessBuilder processBuilder = new ProcessBuilder("sh", "-c", command);
             processBuilder.redirectErrorStream(true);
@@ -46,7 +122,7 @@ public class Runner {
             final BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
             String line;
             while ((line = reader.readLine()) != null) {
-                LOGGER.debug(line);
+                LOGGER.info(line);
             }
 
             int exitCode = process.waitFor();
